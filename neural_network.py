adf074ebad8ce2e31f4bd50db493b539fd6af3f3
lorenzo-delsignore <lorenzo.delsignore94@gmail.com>
2024-07-18 18:12:04 +0200

    Add unique identifiers for the weights of a Neuron

--- neural_network.py
+++ neural_network.py
@@ -5,10 +5,13 @@ from backpropagation import Value
 
 class Neuron:
     def __init__(self, nin):
-        self.weights = [
-            Value(random.uniform(-1, 1), label=f"w_{nin}") for _ in range(nin)
-        ]
-        self.bias = Value(random.uniform(-1, 1), label="bias")
+        self.weights = []
+        for i in range(nin):
+            weight = Value(random.uniform(-1, 1))
+            weight.label = f"w_{i} with id {id(weight)}"
+            self.weights.append(weight)
+        self.bias = Value(random.uniform(-1, 1))
+        self.bias.label = f"bias with id {id(self.bias)}"
 
     def __call__(self, x):
         output = sum((wi * xi for wi, xi in zip(self.weights, x)), self.bias)
