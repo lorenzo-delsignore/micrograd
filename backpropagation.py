a5ffd7ec5f6d903027f00a0be5762887c9086274
lorenzo-delsignore <lorenzo.delsignore94@gmail.com>
2024-07-18 18:11:06 +0200

    Refactor code

--- backpropagation.py
+++ backpropagation.py
@@ -21,7 +21,9 @@ class Value:
         other = (
             other
             if isinstance(other, Value)
-            else Value(other, label=f"number summed to {self.label}")
+            else Value(
+                other, label=f"number summed to {self.label}"
+            )
         )
         sum = Value(self.data + other.data, label="+")
         sum.children.add(self)
@@ -38,7 +40,9 @@ class Value:
         other = (
             other
             if isinstance(other, Value)
-            else Value(other, label=f"number multiplied to {self.label}")
+            else Value(
+                other, label=f"number multiplied to {self.label}"
+            )
         )
         mul = Value(self.data * other.data, label="*")
         mul.children.add(self)
