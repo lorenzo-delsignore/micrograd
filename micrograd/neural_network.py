a8c9429a22ad4efb797dd3dcda7991da5e295c05
Lorenzo Del Signore <66021430+lorenzo-delsignore@users.noreply.github.com>
2024-08-01 21:01:34 +0200

    Add MLP script with command-line options and configure entry point (#3)
    * Add format and save directory options for the computational graph
    
    * Add support for multiple activation functions in MLP
    
    * Add script for MLP with command-line options
    
    * Add entry point for MLP script in pyproject.toml

--- micrograd/neural_network.py
+++ micrograd/neural_network.py
@@ -4,7 +4,7 @@ from micrograd.backpropagation import Value
 
 
 class Neuron:
-    def __init__(self, nin):
+    def __init__(self, nin, activation="relu"):
         self.weights = []
         for i in range(nin):
             weight = Value(random.uniform(-1, 1))
@@ -12,16 +12,20 @@ class Neuron:
             self.weights.append(weight)
         self.bias = Value(random.uniform(-1, 1))
         self.bias.label = f"bias with id {id(self.bias)}"
+        self.activation = activation
 
     def __call__(self, x):
         output = sum((wi * xi for wi, xi in zip(self.weights, x)), self.bias)
-        output = output.tanh()
+        if self.activation == "relu":
+            output = output.relu()
+        elif self.activation == "tanh":
+            output = output.tanh()
         return output
 
 
 class Layer:
-    def __init__(self, nin, nout):
-        self.neurons = [Neuron(nin) for _ in range(nout)]
+    def __init__(self, nin, nout, activation="relu"):
+        self.neurons = [Neuron(nin, activation=activation) for _ in range(nout)]
 
     def __call__(self, x):
         output = [n(x) for n in self.neurons]
@@ -29,9 +33,12 @@ class Layer:
 
 
 class MLP:
-    def __init__(self, nin, nouts):
+    def __init__(self, nin, nouts, activation="relu"):
         in_outs = [nin] + nouts
-        self.layers = [Layer(in_outs[i], in_outs[i + 1]) for i in range(len(nouts))]
+        self.layers = [
+            Layer(in_outs[i], in_outs[i + 1], activation=activation)
+            for i in range(len(nouts))
+        ]
 
     def __call__(self, x):
         for layer in self.layers:
